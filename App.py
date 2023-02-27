209e5b7954c7c064a1fe20d9edac8481e0d80c43
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2023-02-27 15:53:17 +0500

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_functions import st_button, load_css
 st.title("Wasif :red[Mehmood]:sunglasses:")
 st.header("Data Science Intern At :red[Innomatics] Research Labs")
 st.subheader(" Feb 2023")
