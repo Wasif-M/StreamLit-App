a926f151d408d98c946113a8d0ecb1df4ce617a4
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2023-03-06 00:12:20 +0500

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,6 @@ from st_functions import st_button, load_css
 st.title("Wasif :red[Mehmood]:sunglasses:")
 st.header("Data Science Intern At :red[Innomatics] Research Labs")
 st.subheader(" Feb 2023")
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
 
 onClick=st.button("Click Me")
 
